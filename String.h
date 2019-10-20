18582858861cde6070868d891f8f5fe8e81d03b4
Jiri Veverka <xvever12@stud.fit.vutbr.cz>
2019-10-20 15:36:58 +0200

    Strings added

--- String.h
+++ String.h
@@ -1,24 +1,44 @@
 #ifndef STRING_H
 #define STRING_H
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 typedef struct String {
     int lenght;
-    char *chars;
+    char *value;
 } String;
 
 /**
  * Creates a string using malloc.
  */
-String *createString(char **chars);
+String *stringInit(int c);
+
+/**
+ * Adds a character to the string str
+ * [in] string *str: Original string
+ * [in] int c: Character to be added
+ * [out] bool indicating whether operation has succeded or not
+ */
+bool appendCharacter(String *str, int c);
 
 /**
  * Checks whether str1 and str2 are the same.
  */
-bool areSame(String *str1, String *str2);
+//bool areSame(string *str1, string *str2);
 
 /**
  * Deallocates str.
  */
-bool destroy(String *str);
+void stringDestroy(String *str);
+
+/**
+  * Returns length of a input string
+  * [in] char *str: Variable with input string
+  * [out] int with length of the string
+  */
+
+int stringLength(char *str);
+
 #endif
