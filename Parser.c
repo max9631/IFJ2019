278f85d8ff12565f144840a9490e57ea16a7ecf8
Adam Salih <adam.salih@thefuntasty.com>
2019-11-10 00:10:41 +0100

    Add Parser functions

--- Parser.c
+++ Parser.c
@@ -1,2 +1,26 @@
 #include "Parser.h"
 
+BodyNode *parseBody() {
+    return NULL;
+}
+
+FuncNode *parseFunc() {
+    return NULL;
+}
+
+CondNode *parseCond() {
+    return NULL;
+}
+
+WhileNode *parseWhile() {
+    return NULL;
+}
+
+StatementNode *parseStatement() {
+    return NULL;
+}
+
+ExpressionNode *parseExpression() {
+    return NULL;
+}
+
