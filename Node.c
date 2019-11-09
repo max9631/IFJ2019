408e73d167398487507b3d4c02510653bc1fe179
Adam Salih <adam.salih@thefuntasty.com>
2019-11-09 23:57:34 +0100

    Add FunctionNode

--- Node.c
+++ Node.c
@@ -1,2 +1,21 @@
 #include "Node.h"
 
+FuncNode *createFuncNode(String *name, BodyNode *body) {
+    FuncNode *node = (FuncNode *) malloc(sizeof(FuncNode));
+    node->name = name;
+    node->argsCount = 0;
+    node->body = body;
+    return node;
+}
+
+void destroyFuncNode(FuncNode *node) {
+    free(node->name->value);
+    free(node->name);
+    for (int i = 0; i < node->argsCount; i++) {
+        free(node->args[i]->value);
+        free(node->args[i]);
+    }
+    destroyBodyNode(node->body);
+    free(node);
+}
+
--- Node.h
+++ Node.h
@@ -2,4 +2,13 @@
 #define NODE_H
 #include "String.h"
 
+typedef struct _FuncNode {
+    String *name;
+    int argsCount;
+    String **args;
+    BodyNode *body;
+} FuncNode;
+
+FuncNode *createFuncNode(String *name, BodyNode *body);
+void destroyFuncNode(FuncNode *node);
 #endif
