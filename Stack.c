4c220e1097d5309da7602a660e834a79a889f431
Adam Salih <adam.salih@thefuntasty.com>
2019-11-16 17:35:54 +0100

    Add Stack

---
+++ Stack.c
@@ -0,0 +1,29 @@
+#include "Stack.h"
+
+
+Stack *createStack() {
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    stack->items = NULL;
+    stack->count = 0;
+    return stack;
+}
+
+void push(Stack *stack, void *item) {
+    stack->count++;
+    stack->items = realloc(stack->items, stack->count * sizeof(void *));
+    stack->items[stack->count - 1] = item;
+}
+
+void *pop(Stack *stack) {
+    if (stack->count == 0) return NULL;
+    void *item = stack->items[stack->count-1];
+    stack->items[stack->count-1] = NULL;
+    stack->count--;
+    stack->items = realloc(stack->items, stack->count * sizeof(void *));
+    return item;
+}
+
+void destroyStack(Stack *stack) {
+    if (stack->count > 0) free(stack->items);
+    free(stack);
+}
