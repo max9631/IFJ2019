5127ce6e304f7f572929f63dd50f8291c83af5ee
Jiri Veverka <xvever12@stud.fit.vutbr.cz>
2019-11-23 17:00:44 +0100

    generateFunc

--- Generator.c
+++ Generator.c
@@ -21,30 +21,31 @@ void generateBody(Generator *generator, BodyNode *body) {
 }
 
 void generateFunc(Generator *generator, FuncNode *function) {
-    
+    instructionLabel(function->name);
+    generateBody(generator, function->body);
+    instructionReturn();
 }
 
 void generateCond(Generator *generator, CondNode *condition) {
-    
+
 }
 
 void generateWhile(Generator *generator, WhileNode *whileNode) {
-    
+
 }
 
 void generateAssign(Generator *generator, AssignNode *assign) {
-    
+
 }
 
 void generateStatement(Generator *generator, StatementNode *statement) {
-    
+
 }
 
 void generateCall(Generator *generator, CallNode *call) {
-    
+
 }
 
 void generateExpression(Generator *generator, ExpressionNode *expression) {
     // TODO: generate code for type checking. If data types of operand are not compatible, exit with code 4.
 }
-
