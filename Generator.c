e9f46acee1a8ed221db05c8a47b1d76b8ad015e3
Jiri Veverka <xvever12@stud.fit.vutbr.cz>
2019-11-24 15:05:36 +0100

    Fixed various bugs in generators

--- Generator.c
+++ Generator.c
@@ -29,46 +29,63 @@ void generateFunc(Generator *generator, FuncNode *function) {
 void generateCond(Generator *generator, CondNode *condition) {
     String *condTrueLabel = createString("_COND_JMP_LABEL_TRUE_%d", generator->condCount++);
     String *condFalseLabel = createString("_COND_JMP_LABEL_FALSE_%d", generator->condCount);
-    String *trueSymb = createString("bool@true");
-    String *falseSymb = createString("bool@false");
     String *LocalFrame_val = createString("");
 
     generateExpression(generator, condition->condition);
 
-    instructionJumpIfEquls(condTrueLabel, LocalFrame_val, trueSymb);
-    instructionJumpIfNotEqulas(condFalseLabel, LocalFrame_val, falseSymb);
+    instructionJumpIfEqualsStack(condTrueLabel);
+    instructionJumpIfNotEqualsStack(condFalseLabel);
 
+    //if
     instructionLabel(condTrueLabel);
     generateBody(generator, condition->trueBody);
+    instructionPopStack(LocalFrame_val); //TODO: LocalFrame_val -- fix this for global?
+    instructionPopStack(LocalFrame_val); //TODO: LocalFrame_val -- fix this for global?
     instructionReturn();
 
+    //else
     instructionLabel(condFalseLabel);
     generateBody(generator, condition->falseBody);
+    instructionPopStack(LocalFrame_val); //TODO: LocalFrame_val -- fix this for global?
+    instructionPopStack(LocalFrame_val); //TODO: LocalFrame_val -- fix this for global?
     instructionReturn();
 }
 
 void generateWhile(Generator *generator, WhileNode *whileNode) {
     String *whileLabel = createString("_WHILE_JMP_LABEL_%d", generator->whileCount++);
+    String *whileLabel_end = createString("_WHILE_END_JMP_LABEL_%d", generator->whileCount);
     String *LocalFrame_val = createString("");
-    String *trueSymb = createString("bool@true");
 
     instructionLabel(whileLabel);
+
+    //Check the while condition
     generateExpression(generator, whileNode->condition);
-    instructionPopStack(LocalFrame_val);
-    instructionJumpIfEquls(whileLabel, LocalFrame_val, trueSymb);
+    instructionJumpIfEqualsStack(whileLabel_end);
+
+    //Generate while body
+    generateBody(generator, whileNode->body);
+    instructionJump(whileLabel);
+
+    //While end
+    instructionLabel(whileLabel_end);
+    instructionPopStack(LocalFrame_val); //TODO: LocalFrame_val -- fix this for global?
+    instructionPopStack(LocalFrame_val); //TODO: LocalFrame_val -- fix this for global?
     instructionReturn();
 }
 
 void generateAssign(Generator *generator, AssignNode *assign) {
-    
+
 }
 
 void generateStatement(Generator *generator, StatementNode *statement) {
-    
+
 }
 
 void generateCall(Generator *generator, CallNode *call) {
-    
+    for(int i = 0; i < call->argsCount; i++){
+        generateExpression(generator, call->expressions[i]);
+    }
+    instructionCall(call->identifier);
 }
 
 void generateExpression(Generator *generator, ExpressionNode *expression) {
